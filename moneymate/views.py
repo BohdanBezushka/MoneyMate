a69c9f80b713c7b42af6353a76e247a90799a056
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-04-15 22:32:26 +0000

    Add index url to url file in expensesapp

--- moneymate/views.py
+++ moneymate/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here:
 
 def homePage(request):
-   return render(request, 'moneymate/index.html')
+    # This is the main page that will be displayed to the user.
+    # The function has been exported to the 'expensesapp' 
+    # project in the 'urls.py' file in order to visualize the HTML content.
+    return render(request, 'moneymate/index.html')
