c924aac7e504c86e8310c21443013109141a089a
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-04-17 20:32:35 +0000

    Install Django-Allauth

--- expensesapp/settings.py
+++ expensesapp/settings.py
@@ -42,9 +42,18 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'django.contrib.staticfiles',
     'moneymate',
 ]
+
+SIDE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
