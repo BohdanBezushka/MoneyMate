00e62402c1ec7c8f3659b760813e72643ac61537
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-04-21 19:13:40 +0000

    Add Expense and Category in models

--- moneymate/models.py
+++ moneymate/models.py
@@ -1,4 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+from django.utils.timezone import now
+
+# Create your models here:
+
+# This is the database for user expenses.
+class addExpense(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    date = models.DateField(default=now)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=50)
+    description = models.TextField(max_length=300)
+
+    def __str__(self):
+        return self.category
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
