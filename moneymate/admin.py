c5971971e3c6be69354b0b296de47abd659d377d
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-04-24 19:58:13 +0000

    Update the admin file with the new models

--- moneymate/admin.py
+++ moneymate/admin.py
@@ -1,20 +1,43 @@
 from django.contrib import admin
 from django.db.models import Sum
-from .models import Expense, Category, Origin, Income
+from .models import Expense, Category, Origin, Income, Currency
 
 # Register your models here:
+
+# Category and Expense:
 class ExpenseAdmin(admin.ModelAdmin):
     list_display = ('user', 'date', 'amount', 'category', 'description')
     list_filter = ('user', 'category')
     ordering = ('user','category')
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name')
+    list_filter = ('user', 'name')
+    ordering = ('user','name')
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Expense, ExpenseAdmin)
 
+# Origin and Income:
 class IncomeAdmin(admin.ModelAdmin):
     list_display = ('user', 'date', 'amount', 'origin', 'description')
     list_filter = ('user', 'origin')
     ordering = ('user','origin')
 
-admin.site.register(Origin)
+class OriginAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name')
+    list_filter = ('user', 'name')
+    ordering = ('user','name')
+
+admin.site.register(Origin, OriginAdmin)
 admin.site.register(Income, IncomeAdmin)
+
+
+# Currency: 
+
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = ('user', 'currency', 'abbreviation', 'symbol')
+    list_filter = ('user', 'currency', 'abbreviation', 'symbol')
+    ordering = ('user', 'currency', 'abbreviation', 'symbol')
+
+admin.site.register(Currency, CurrencyAdmin)
