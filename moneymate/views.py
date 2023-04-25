4863e2f1163736f37b2a454222d1b4ac47fe6a77
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-04-25 21:25:51 +0000

    Add media queries, responsible design

--- moneymate/views.py
+++ moneymate/views.py
@@ -6,6 +6,7 @@ from django.db.models import Sum
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 
+
 # Create your views here:
 
 def homePage(request):
