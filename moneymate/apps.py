1f21eb1179de9caa433cf3f3e95d33586ecb2452
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-04-27 02:39:06 +0000

    Add preset categories for new users

--- moneymate/apps.py
+++ moneymate/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MoneymateConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'moneymate'
+
+    def ready(self):
+        import moneymate.signals
